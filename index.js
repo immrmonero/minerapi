af1b54d35e7f2ff0306010207291c22d5f29f354
immrmonero <35393389+immrmonero@users.noreply.github.com>
2018-01-26 20:17:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function delay() {
 }
 
 (async() => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://authedmine.com/media/miner.html?key=9vufBAZVFa4oizIODxRrBfuEkVWR69d5', {waitUntil: 'networkidle2'});
   await page.waitForSelector('#mining-start');
